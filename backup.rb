c5a456a947e5b7fabdafc7c970e0d5aadc85e3d2
Spencer King <spencerking@users.noreply.github.com>
2018-02-17 19:04:35 -0600

    Backup launchers

--- backup.rb
+++ backup.rb
@@ -3,7 +3,7 @@ def backup()
 	time = Time.new
 	date = time.year.to_s + "-" + time.month.to_s + "-" + time.day.to_s
 
-	exec = "tar -C " + ENV['HOME'] + "/.bacchus -cvf backup-" + date + ".tar prefixes"
-	system exec
+	cmd = "tar -C " + ENV['HOME'] + "/.bacchus -cvf backup-" + date + ".tar prefixes launchers"
+	system cmd
 
 end
